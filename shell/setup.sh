f48581055740df03f988643b7ef23a6ec322e78f
Gong Yunqing <gongyq10@gmail.com>
2020-01-23 14:32:41 +0800

    feat,fix: add some alias; zshrc

--- shell/setup.sh
+++ shell/setup.sh
@@ -10,11 +10,15 @@ fi
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
 # copy official rcfile
+
+pushd $HOME
 shopt -s extglob
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/!(README.md); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+popd
 
 # copy customised rcfile
 cp .* $HOME
 cat patchrc >> $HOME/.zshrc
+printf "%s\n" "$1" | sudo --stdin chsh -s /bin/zsh
